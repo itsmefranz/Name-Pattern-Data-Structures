63452f3068aa6f669250b2a4df29b13cd7769218
itsmefranz <daniellafrancine.velasquez@gmail.com>
2022-10-22 12:00:32 +0800

    stated 3 conditions for the first letter of my nickname

--- printing-name-pattern.py
+++ printing-name-pattern.py
@@ -1,5 +1,9 @@
 for row in range(7):
     for col in range(5):
+        if (col==0) or (col==4 and (row!=0 and row!=6)) or ((row==0 or row==6) and (col>0 and col<4)) 
+
+for row in range(7):
+    for col in range(5):
         #the indicated number in the range will be the designated number of characters in the output#
         if ((col==0 or col==4) and row!=0) or (row==0 or row==3) and ((col>0 and col<4)):
         #used conditions to where to put stars (asterisk) in columns and rows in the letter printed 
