89ce7f43308bf89d4f701c5c8d3d1faca5cd4a10
itsmefranz <daniellafrancine.velasquez@gmail.com>
2022-10-22 11:49:45 +0800

    settled conditions to where stars, asterisk rather would appear using columns and rows along with their ranges

--- printing-name-pattern.py
+++ printing-name-pattern.py
@@ -1,3 +1,5 @@
 for row in range(7):
     for col in range(5):
-        #the indicated number in the range will be the designated number of characters in the output
+        #the indicated number in the range will be the designated number of characters in the output#
+        if (c(ol==0 or col==4) and row!=0) or (row==0 or row==3) and ((col>0 and col<4))
+        #used conditions to where to put stars (asterisk) in columns and rows in the letter printed 
